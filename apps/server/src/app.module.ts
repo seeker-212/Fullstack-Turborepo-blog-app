0d4de30a58a3ac07797f5e5530e3510aa646c8a9
Henry <henryugwu565@gmail.com>
2026-06-19 14:25:23 +0100

    Update: Integrating GraphQl into my app

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [PrismaModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
+    }),
+    PrismaModule,
+  ],
 })
 export class AppModule {}
