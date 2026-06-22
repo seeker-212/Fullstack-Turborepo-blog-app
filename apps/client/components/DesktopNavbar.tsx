7ad897c98f8bc8748822ac399e4c8b69e1767155
Henry <henryugwu565@gmail.com>
2026-06-22 20:11:08 +0100

    Update: Done with the hero section: TODO: working on the navbar

---
+++ apps/client/components/DesktopNavbar.tsx
@@ -0,0 +1,13 @@
+import { PropsWithChildren } from "react"
+
+type Props = PropsWithChildren
+
+const DesktopNavbar = (props: Props) => {
+  return (
+    <div>
+      Desktop Navbar
+    </div>
+  )
+}
+
+export default DesktopNavbar
--- apps/client/components/hero.tsx
+++ apps/client/components/hero.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Hero() {
   return (
     <div className="bg-linear-to-br from-sky-500 to-indigo-500 text-white">
-      <div className="container mx-auto flex flex-col md:flex-row items-center px-3">
+      <div className="container mx-auto flex flex-col md:flex-row items-center px-3 pt-12">
         {/* LEFT SECTION */}
         <div className="w-full md:w-1/2 text-center md:text-left">
           <p className="uppercase tracking-wider text-sm md:text-base">
@@ -22,18 +22,17 @@ export default function Hero() {
         </div>
 
         {/* RIGHT SECTION */}
-        <div className="flex w-full justify-center md:w-1/2">
-          <Image
+        <div className="flex w-full justify-center md:w-3/5 p-7">
+          <img
             src="/Hero.png"
             alt="Hero section illustration"
-            width={600}
-            height={500}
-            priority
-            className="h-auto w-full max-w-md md:max-w-lg"
+            className="w-full md:w-3/4 z-30"
           />
         </div>
       </div>
-      <div className='relative'></div>
+      <div className='relative lg:mt-24 direction-reverse'>
+       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" className='rotate-180 -mt-33'><g fill="#FFFFFF"><path d="M0 1v99c134.3 0 153.7-99 296-99H0Z" opacity=".5"></path><path d="M1000 4v86C833.3 90 833.3 3.6 666.7 3.6S500 90 333.3 90 166.7 4 0 4h1000Z" opacity=".5"></path><path d="M617 1v86C372 119 384 1 196 1h421Z" opacity=".5"></path><path d="M1000 0H0v52C62.5 28 125 4 250 4c250 0 250 96 500 96 125 0 187.5-24 250-48V0Z"></path></g></svg>
+      </div>
     </div>
   );
 }
